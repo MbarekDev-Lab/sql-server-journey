1a4e8a8a625cb3140bd18bc471eaa51ef5cadfbb
M'barek Benraiss <81750067+MbarekDev-Lab@users.noreply.github.com>
2025-04-27 21:10:15 +0200

    Rolling Total!

--- learningT-SQL-Journey.sql
+++ learningT-SQL-Journey.sql
@@ -118,6 +118,14 @@ from tblEmployee as E join tblAttendance as A
 on E.EmployeeNumber = A.EmployeeNumber
 WHERE A.AttendanceMonth < '20150101';
 
+-- Range
+select A.EmployeeNumber, A.AttendanceMonth, A.NumberAttendance, 
+SUM(A.NumberAttendance) over(PARTITION BY E.EmployeeNumber ORDER BY A.AttendanceMonth 
+ROWS BETWEEN 1 PRECEDING AND 1 FOLLOWING) as RollingTotal
+from tblEmployee as E join tblAttendance as A
+on  E.EmployeeNumber =  A.EmployeeNumber
+
+
 
 
 
