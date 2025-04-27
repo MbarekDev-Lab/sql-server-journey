236b0e939ea44e54ed73cae53730d945a6141154
M'barek Benraiss <81750067+MbarekDev-Lab@users.noreply.github.com>
2025-04-27 20:41:13 +0200

    windowed running totals, partitioning, and ordering inside windows!

--- learningT-SQL-Journey.sql
+++ learningT-SQL-Journey.sql
@@ -49,10 +49,7 @@ WHERE EXISTS (
     WHERE a.EmployeeNumber = e.EmployeeNumber
 );
 
-
 -- OVER 
-
-
 SELECT e.EmployeeNumber,
   YEAR(AttendanceMonth) AS AttendanceYear, 
   SUM(a.NumberAttendance) OVER() AS GrandTotalAttendance
@@ -84,8 +81,14 @@ convert(decimal(18,7),A.NumberAttendance) / sum(A.NumberAttendance) over() * 100
 from tblEmployee as E join tblAttendance as A
 on E.EmployeeNumber = A.EmployeeNumber
 
-select sum(NumberAttendance) from tblAttendance;
+-- 	SUM over each group separately vs SUM over all rows
+SELECT sum(NumberAttendance) FROM tblAttendance;
+SELECT SUM(NumberAttendance) OVER() AS TotalAttendances
+FROM tblAttendance;
 
+SELECT EmployeeNumber, AttendanceMonth, NumberAttendance,
+ROW_NUMBER() OVER(ORDER BY AttendanceMonth) AS RowNum
+FROM tblAttendance;
 
 --mixing window functions after GROUP BY,
 --like in a CTE (Common Table Expression)
@@ -96,8 +99,30 @@ WITH AttendancePerEmployee AS (
     GROUP BY EmployeeNumber, YEAR(AttendanceMonth)
 )
 SELECT EmployeeNumber, Year, TotalAttendance,
-    SUM(TotalAttendance) OVER() AS GrandTotal
+    SUM(TotalAttendance) OVER() AS GrandTotal 
 FROM AttendancePerEmployee;
+
+SELECT A.EmployeeNumber, A.AttendanceMonth, A.NumberAttendance,
+SUM(A.NumberAttendance) OVER(PARTITION BY A.EmployeeNumber, YEAR(AttendanceMonth) ORDER BY A.AttendanceMonth DESC) AS RuningTotal
+--,CONVERT(decimal(18,7),A.NumberAttendance) / SUM(A.NumberAttendance) OVER(PARTITION BY A.EmployeeNumber) * 100.0000 AS PercentageAttendance
+FROM tblEmployee AS E JOIN tblAttendance AS A 
+ON E.EmployeeNumber = A.EmployeeNumber
+--WHERE A.AttendanceMonth < '20150101';
+--WHERE year(A.AttendanceMonth) = '2014';
+
+--Partition by and Order by
+select A.EmployeeNumber, A.AttendanceMonth, A.NumberAttendance, 
+sum(A.NumberAttendance) over(PARTITION BY E.EmployeeNumber) as SumAttendance,
+convert(money,A.NumberAttendance) / sum(A.NumberAttendance) over(PARTITION BY E.EmployeeNumber) * 100 as PercentageAttendance
+from tblEmployee as E join tblAttendance as A
+on E.EmployeeNumber = A.EmployeeNumber
+WHERE A.AttendanceMonth < '20150101';
+
+
+
+
+
+
 
 
 
