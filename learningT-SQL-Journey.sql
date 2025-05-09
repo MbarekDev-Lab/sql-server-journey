db1f80bee6ca92ebc76226b4adbe5b43f5e05aaf
M'barek Benraiss <81750067+MbarekDev-Lab@users.noreply.github.com>
2025-05-09 07:50:51 +0200

    self join

--- learningT-SQL-Journey.sql
+++ learningT-SQL-Journey.sql
@@ -1266,6 +1266,24 @@ PIVOT (
 ) AS myPvt
 ORDER BY TheYear;
 
+--17. Self Joins
+
+BEGIN TRAN;
+ALTER TABLE tblEmployee 
+ADD Manager INT;
+GO
+UPDATE tblEmployee
+SET Manager = ((EmployeeNumber - 123) / 10) + 123
+WHERE EmployeeNumber > 123;
+SELECT 
+    E.EmployeeNumber, E.EmployeeFirstName, E.EmployeeLastName,
+    M.EmployeeNumber AS ManagerNumber, M.EmployeeFirstName AS ManagerFirstName,
+    M.EmployeeLastName AS ManagerLastName
+FROM tblEmployee AS E
+LEFT JOIN tblEmployee AS M
+ON E.Manager = M.EmployeeNumber;
+-- COMMIT TRAN; -- Keep the changes
+ROLLBACK TRAN; -- Undo everything
 
 
 
